f37af1d45c1fb5c541f542d1a96974d4a9dd3bc4
urvi koladiya <50900652+cp-urvi-k@users.noreply.github.com>
2021-02-04 10:42:37 +0530

    improve logic

--- StretchableScrollView/StretchableScrollViewController.swift
+++ StretchableScrollView/StretchableScrollViewController.swift
@@ -18,27 +18,40 @@ class StretchableScrollViewController: UIViewController {
 }
 
 extension StretchableScrollViewController: UIScrollViewDelegate {
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        let actualPosition = scrollView.panGestureRecognizer.translation(in: scrollView.superview)
-        if ((actualPosition.y > 1 && actualPosition.y < 10) && (scrollView.contentOffset.y == 0)) {
-            if scrollView.contentOffset.y == 0 {
-                UIView.animate(withDuration: 0.7, animations: {
-                    self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 3.5
-                })
+    
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.contentOffset.y > -50  && scrollView.contentOffset.y < -1 {
+            
+            let difference = self.scrollView.contentSize.height - self.scrollView.contentSize.height / 3.5
+            
+            if difference >= scrollView.frame.size.height {
+                self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 3.5
+            }else {
+                self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
             }
-        } else if ((actualPosition.y > 10 && actualPosition.y < 20) && (scrollView.contentOffset.y == 0)) {
-            if scrollView.contentOffset.y == 0 {
-                UIView.animate(withDuration: 0.7, animations: {
-                    self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 2.5
-                })
+        }else if scrollView.contentOffset.y > -100  && scrollView.contentOffset.y < -50 {
+            
+            let difference = self.scrollView.contentSize.height - self.scrollView.contentSize.height / 2.5
+            
+            if difference >= scrollView.frame.size.height {
+                self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 2.5
+            }else {
+                self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
             }
-        } else if ((actualPosition.y > 20 ) && (scrollView.contentOffset.y == 0)) {
-            if scrollView.contentOffset.y == 0 {
-                UIView.animate(withDuration: 0.7, animations: {
-                    self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 2
-                })
+        } else if scrollView.contentOffset.y > -150 && scrollView.contentOffset.y < -100{
+            
+            let difference = self.scrollView.contentSize.height - self.scrollView.contentSize.height / 1.8
+            
+            if difference >= scrollView.frame.size.height {
+                self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 1.8
+            }else {
+                self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
             }
-        }else
+        } else
+        if scrollView.contentOffset.y < -150 {
+            self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
+        } else
         {
             self.scrollView.contentInset.top = 0
         }
