44a9a8dde521cc675bb14c0bbf7462b5f5427860
cp-urvi-k <urvi.k@canopas.com>
2021-02-18 18:28:28 +0530

    refactor logic

--- StretchableScrollView/StretchableScrollViewController.swift
+++ StretchableScrollView/StretchableScrollViewController.swift
@@ -21,40 +21,19 @@ extension StretchableScrollViewController: UIScrollViewDelegate {
     
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if scrollView.contentOffset.y > -50  && scrollView.contentOffset.y < -1 {
-            
-            let difference = self.scrollView.contentSize.height - self.scrollView.contentSize.height / 3.5
-            
-            if difference >= scrollView.frame.size.height {
-                self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 3.5
-            }else {
-                self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
-            }
-        }else if scrollView.contentOffset.y > -100  && scrollView.contentOffset.y < -50 {
-            
-            let difference = self.scrollView.contentSize.height - self.scrollView.contentSize.height / 2.5
-            
-            if difference >= scrollView.frame.size.height {
-                self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 2.5
-            }else {
-                self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
-            }
-        } else if scrollView.contentOffset.y > -150 && scrollView.contentOffset.y < -100{
-            
-            let difference = self.scrollView.contentSize.height - self.scrollView.contentSize.height / 1.8
-            
-            if difference >= scrollView.frame.size.height {
-                self.scrollView.contentInset.top = -self.scrollView.contentSize.height / 1.8
-            }else {
-                self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
-            }
-        } else
-        if scrollView.contentOffset.y < -150 {
-            self.scrollView.contentInset.top = -self.scrollView.contentSize.height + scrollView.frame.size.height + 20
-        } else
-        {
+        
+        if scrollView.contentOffset.y < 0 {
+            self.scrollView.contentInset.top = estimetedScrollingPoint(offsetY: scrollView.contentOffset.y)
+        } else {
             self.scrollView.contentInset.top = 0
         }
+    }
+    
+    func estimetedScrollingPoint(offsetY: CGFloat) -> CGFloat {
+        
+        let estimatedMax = (offsetY * 100) / (self.view.frame.size.height / 3)
+        let offset = (estimatedMax * (scrollView.contentSize.height - self.view.frame.size.height)) / 100
+        return offset
     }
 }
 
